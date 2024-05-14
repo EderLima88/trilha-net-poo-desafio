58a7cfc5c72f36d263be43239016b46063d95ef6
Eder de Lima <eder.ebl@gmail.com>
2024-05-14 17:16:38 -0300

    Primeiro commit para entrega projeto concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Usando o smartphone NOKIA");
+Smartphone nokia = new Nokia("1111","flip","3333", 5555);
+nokia.InstalarAplicativo("watsap");
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+Console.WriteLine("Usando o smartphone IPHONE");
+Smartphone Iphone = new Iphone("222","telapequena","7777", 8888);
+Iphone.InstalarAplicativo("calculadora");
+Iphone.Ligar();
+Iphone.ReceberLigacao();
+
+
